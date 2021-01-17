9d0a631dea50f0499b2b8c65eb9bcba0d8537e49
Andrei Lapets <lapets@gmail.com>
2021-01-16 20:24:33 -0500

    Update formatting/naming conventions, comments, and tests.

--- flats/flats.py
+++ flats/flats.py
@@ -1,25 +1,25 @@
-"""Library for flattening nested values of container types.
+"""Library for flattening nested instances of container types.
 
 Python library for common functionalities related to flattening
-nested values of container types.
+nested instances of container types.
 """
-import types
+from types import GeneratorType
 import doctest
 
-def _is_container(v):
-    return any(isinstance(v, t) for t in [
-        types.GeneratorType,
-        list,
-        set, frozenset,
-        tuple
-    ])
+def _is_container(instance):
+    return isinstance(instance, (
+        tuple, list, set, frozenset,
+        GeneratorType
+    ))
 
-def flats(value, depth = 1):
+def flats(xss, depth=1):
     """
-    Flatten a value that consists of nested values
-    of some built-in container type(s).
+    Flatten an instance that consists of nested values
+    of a container type.
 
     >>> list(flats([[1,2,3],[4,5,6,7]]))
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> list(flats([(1,2,3), (4,5,6,7)]))
     [1, 2, 3, 4, 5, 6, 7]
     >>> list(flats([[[1,2],3],[4,5,6,7]], depth=2))
     [1, 2, 3, 4, 5, 6, 7]
@@ -29,40 +29,39 @@ def flats(value, depth = 1):
     [[1, 2], 3, 4, 5, 6, 7]
     >>> list(flats([[[1,2],3],[4,5,6,7]], depth=0))
     [[[1, 2], 3], [4, 5, 6, 7]]
-    >>> list(flats([[[1,2],3],[4,5,6,7]], depth=float('inf')))
+    >>> list(flats([[[1,[2]],3],[4,[[[5]]],6,7]], depth=float('inf')))
     [1, 2, 3, 4, 5, 6, 7]
     """
     if depth == 1: # Most common case is first for efficiency.
-        for vs in value:
-            if _is_container(vs):
-                for v in vs:
-                    yield v
+        for xs in xss:
+            if _is_container(xs):
+                for x in xs:
+                    yield x
             else:
-                yield vs
-    elif depth == 0:
-        for v in value:
-            yield v
+                yield xs
+    elif depth == 0: # For consistency, base case is also a generator.
+        for xs in xss:
+            yield xs
     else: # General recursive case.
-
-        for vs in value:
+        for xs in xss:
             if isinstance(depth, int) and depth >= 1:
-                if _is_container(vs):
-                    for v in flats(vs, depth=depth-1):
-                        yield v
+                if _is_container(xs):
+                    for x in flats(xs, depth=(depth - 1)):
+                        yield x
                 else:
-                    yield vs
+                    yield xs
             elif depth == float('inf'):
-                if _is_container(vs):
-                    for v in flats(vs, depth = float('inf')):
-                        yield v
+                if _is_container(xs):
+                    for x in flats(xs, depth=float('inf')):
+                        yield x
                 else:
-                    yield vs
+                    yield xs
             elif not isinstance(depth, int) and depth == float('inf'):
-                raise TypeError("depth must be an integer or infinity")
+                raise TypeError('depth must be an integer or infinity')
             elif isinstance(depth, int) and depth < 0:
-                raise ValueError("depth must be a non-negative integer")
+                raise ValueError('depth must be a non-negative integer or infinity')
             else:
-                raise ValueError("invalid depth value")
+                raise ValueError('depth value is invalid')
 
 if __name__ == "__main__": 
     doctest.testmod()
